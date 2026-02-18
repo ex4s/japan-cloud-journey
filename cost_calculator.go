5cf680bebfa1d282ee69f6c2c56fa9689a4ad4ea
Thomas S <thomas@cavallone.fr>
2026-02-18 17:12:11 +0100

    Update cost_calculator.go
    ajout des commentaires

--- cost_calculator.go
+++ cost_calculator.go
@@ -3,16 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	// 1. Définition des variables
+	// On ajoute .0 à chaque chiffre pour que Go comprenne que ce sont des nombres décimaux (float64)
 	nombreHeures := 730.0
 	
 	prixAzureParHeure := 0.0960 //D2s_v3
 	prixAWSParHeure := 0.0416 //t3 medium
 	prixGCPParHeure := 0.0670 //e2-standard-2
 
+	// 2. Calcul du coût total
 	coutTotalAzure := nombreHeures * prixAzureParHeure
 	coutTotalAWS := nombreHeures * prixAWSParHeure
 	coutTotalGCP := nombreHeures * prixGCPParHeure
 
+	// 3. Affichage des résultats
 	fmt.Println("===========================================")
 	fmt.Println("   COMPARATEUR DE COÛT CLOUD (MENSUEL)    ")
 	fmt.Println("===========================================")
@@ -22,7 +26,7 @@ func main() {
 	fmt.Printf("Estimation Coût GCP   : %.2f USD\n", coutTotalGCP) // Corrigé ici
 	fmt.Println("-------------------------------------------")
 
-	// Logique simplifiée : On cherche le plus petit des trois
+	// 4. Logique de décision
 	if coutTotalGCP < coutTotalAWS && coutTotalGCP < coutTotalAzure {
 		fmt.Println("VERDICT : GCP est le plus économique.")
 	} else if coutTotalAWS < coutTotalAzure {
